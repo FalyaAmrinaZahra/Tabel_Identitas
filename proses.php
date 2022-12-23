d5774be45495733d99027fb43ac4938350a8867f
FalyaAmrinaZahra <80408125+FalyaAmrinaZahra@users.noreply.github.com>
2022-12-23 19:39:49 +0700

    Add files via upload

---
+++ proses.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+
+<!-- <h1>Nama = <?= $nama ?></h1>
+<h1>Alamat = <?= $alamat?></h1> --> 
+
+<table border="1">
+    <tr>
+        <th>NIM</th>
+        <th>NAMA</th>
+        <th>ALAMAT</th>
+        <th>JENIS KELAMIN</th>
+        <th>TANGGAL LAHIR</th>
+        <th>TEMPAT LAHIR</th>
+        <th>JURUSAN</th>
+        <th>TAHUN MASUK</th>
+        
+        
+    </tr> 
+    <tr>
+        <td>20200040095</td>
+        <td>FalyaAmrinaZahra </td>
+        <td>Kebonrandu </td>
+        <td>Perempuan </td>
+        <td>22Maret2022</td>
+        <td>Sukabumi </td>
+        <td>TeknikInformatika </td>
+        <td>2020</td>
+    </tr>
+</table> 
